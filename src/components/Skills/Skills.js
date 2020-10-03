6f936c08c7372c635bb4359d7d47f159a8abff67
AlexCrosby <AlexCrosby@live.co.uk>
2020-10-03 14:04:56 +0100

    Updated skills

--- src/components/Skills/Skills.js
+++ src/components/Skills/Skills.js
@@ -1,27 +1,34 @@
 import React from "react";
 import "./Skills.css"
 
+
+const skillItems = ['Java',
+    'JavaFX',
+    'Python',
+    'PostgreSQL',
+    'MongoDB',
+    'Linux',
+    'TensorFlow',
+    'Git',
+    'HTML',
+    'CSS',
+    'JavaScript',
+    'React',
+    'Node.js',
+    'Google Cloud',
+    'Amazon Web Services',
+    'Arduino']
+
 const Skills = () => {
 
     return (<div id={"skills"} className={"skills-container"}>
             <h1>skills.</h1>
 
 
-            <div className={"skills-items"}>
-                <div className={"skills-items-a"}>
-                    <ul>
-                        <li>a</li>
-                        <li>b</li>
-                    </ul>
-                </div>
-                <div className={"skills-items-b"}>
-                    <ul>
-                        <li>c</li>
-                        <li>d</li>
-                    </ul>
-                </div>
-
-
+            <div className={"skills-items-body"}>
+                <ul className={"skill-items"}>
+                    {skillItems.map(item => (<li className={"skill"} key={item}>{item}</li>))}
+                </ul>
             </div>
         </div>
     )
